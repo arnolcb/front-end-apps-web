b9254db4c35de56f3ac763828bb0d774b2f2c1ad
Oliver Tuesta <oliver.jtuesta@gmail.com>
2023-04-04 21:11:23 -0500

    feat: header

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+    <h1 class="title">{{ title }}</h1>
+    <nav class="nav">
+      <ul class="nav-list">
+        <li class="nav-item">Home</li>
+        <li class="nav-item">About</li>
+        <li class="nav-item">Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  data() {
+    return {
+      title: 'My Website'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: #f0f0f0;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.title {
+  font-size: 24px;
+}
+
+.nav-list {
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+}
+
+.nav-item {
+  margin-right: 10px;
+}
+</style>
+
+
